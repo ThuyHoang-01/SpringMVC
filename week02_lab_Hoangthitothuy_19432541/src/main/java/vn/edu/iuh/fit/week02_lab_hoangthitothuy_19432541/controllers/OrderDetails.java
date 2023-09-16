711916a266cc666517e73082fd080ee4332532f5
Hoàng Thị Tố Thùy <httthuy455@gmail.com>
2023-09-16 09:00:28 +0700

    create project week 2
    create project week 2

---
+++ week02_lab_Hoangthitothuy_19432541/src/main/java/vn/edu/iuh/fit/week02_lab_hoangthitothuy_19432541/controllers/OrderDetails.java
@@ -0,0 +1,20 @@
+package vn.edu.iuh.fit.week02_lab_hoangthitothuy_19432541.controllers;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "order_details")
+public class OrderDetails {
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
